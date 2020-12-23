c3c8eebe98d89eff954fff04207db40882f64d00
Eric_Yue <eric_yue@dell.com>
2020-12-23 10:10:44 +0800

    feat: get seatcode by token added

--- db_middleware/reservationRoutes.py
+++ db_middleware/reservationRoutes.py
@@ -24,7 +24,7 @@ api = Namespace('Reservation', description='Reservation')
 class getAvailable(Resource):
     @api.doc(description="Get all available seats")
     def get(self):
-        resv = db.session.query(Reservation.seat_id).filter(Reservation.date == date.today()).filter(Reservation.release_time <= datetime.now().time()).all()
+        resv = db.session.query(Reservation.seat_id).filter(Reservation.date == date.today()).filter(Reservation.release_time >= datetime.now().time()).all()
         resv = [x[0] for x in resv]
         print(resv)
         query = db.session.query(Seat).filter(Seat.id.notin_(resv)).all()
@@ -35,7 +35,7 @@ class getAvailable(Resource):
 class getAvailable(Resource):
     @api.doc(description="Get all available seats filter by area")
     def get(self, aid):
-        resv = db.session.query(Reservation.seat_id).filter(Reservation.date == date.today()).filter(Reservation.release_time <= datetime.now().time()).all()
+        resv = db.session.query(Reservation.seat_id).filter(Reservation.date == date.today()).filter(Reservation.release_time >= datetime.now().time()).all()
         query = db.session.query(Seat).filter(Seat.aid == aid).filter(Seat.id.notin_([x[0] for x in resv])).all()
         return [jsontifySeat(s) for s in query], 200
 
@@ -44,7 +44,7 @@ class getAvailable(Resource):
 class getAvailable(Resource):
     @api.doc(description="Get all available seat count filter by area")
     def get(self, aid):
-        resv = db.session.query(Reservation.seat_id).filter(Reservation.date == date.today()).filter(Reservation.release_time <= datetime.now().time()).all()
+        resv = db.session.query(Reservation.seat_id).filter(Reservation.date == date.today()).filter(Reservation.release_time >= datetime.now().time()).all()
         query = db.session.query(Seat).filter(Seat.aid == aid).filter(Seat.id.notin_([x[0] for x in resv])).count()
         available = db.session.query(Seat).filter(Seat.aid == aid).count()
         return {
@@ -53,14 +53,12 @@ class getAvailable(Resource):
                }, 200
 
 
-resource_fields = api.model('reservations_form', reservations_form)
 
 
 @api.route('/release')
 class getAvailable(Resource):
-    @api.doc(description="Create a reservation")
+    @api.doc(description="Release a reservation")
     @jwt_required
-    @api.expect(resource_fields)
     @api.param("Authorization", _in='header')
     def post(self):
         try:
@@ -78,10 +76,13 @@ class getAvailable(Resource):
         except:
             return {"message": "bad payload"}, 400
 
+resource_fields = api.model('reservations_form', reservations_form)
+
 @api.route('/reservations')
 class getAvailable(Resource):
     @api.doc(description="Create a reservation")
     @jwt_required
+    @api.expect(resource_fields)
     @api.param("Authorization", _in='header')
     def post(self):
         try:
@@ -105,5 +106,29 @@ class getAvailable(Resource):
                 return reservations.id, 200
             else:
                 return {"message": "The seat has been reserved"}, 400
+        except:
+            return {"message": "bad payload"}, 400
+
+@api.route('/usar/seat')
+class getAvailable(Resource):
+    @api.doc(description="get seat code")
+    @jwt_required
+    @api.param("Authorization", _in='header')
+    def get(self):
+        try:
+            current_user = get_jwt_identity()
+            user = User.query.filter_by(username=current_user).first()
+
+            exists_result = db.session.query(Reservation).with_lockmode("update").filter(
+                Reservation.user_id == user.id).filter(Reservation.date == date.today()).filter(Reservation.release_time >= datetime.now().time()).first()
+            if exists_result:
+                s = db.session.query(Seat).filter(Seat.id == exists_result.seat_id).first()
+                return {
+                           "seat": s.seatCode
+                       }, 200
+            else:
+                return {
+                           "seat": None
+                       }, 200
         except:
             return {"message": "bad payload"}, 400
